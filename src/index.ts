896c95e07e4456a71f8b4678105b4a2b6c1207e0
mudssky <mudssky@gmail.com>
2022-06-01 16:19:00 +0800

    ✨ feat: 移除初始的foo demo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as TitledCard } from './components/Card/TitledCard'
 export { default as ColFilteredTable } from './components/Table/ColFilteredTable'
 export { default as Dropdown } from './components/Dropdown/Dropdown'
-export { default as Foo } from './components/Foo'
